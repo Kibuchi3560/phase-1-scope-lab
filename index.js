87af706b581025f26a03b646745a3a16f62f0a5e
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-09-26 08:24:29 +0300

    scope lab Changes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+const LeastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer () {
+    LeastFavoriteCustomer = "some new value";
+}
